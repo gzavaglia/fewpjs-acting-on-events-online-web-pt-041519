05745f37c1472074064f5b51e541586185eb0593
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-11-04 18:29:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,20 @@ function moveDodgerLeft() {
   }
 }
 
+function moveDodgerRight() {
+  let rightNumbers = dodger.style.right.replace("px", "");
+  let right = parseInt(rightNumbers, 10);
+ 
+  if (right > 0) {
+    dodger.style.right = `${right + 1}px`;
+  }
+}
+
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
+  }
+  else if (e.key === "ArrowRight"){
+    moveDodgerRight();
   }
 });
