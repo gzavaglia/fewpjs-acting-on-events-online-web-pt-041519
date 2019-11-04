523fe77ecb0e5123bc048483af9818e358abf7cf
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-11-04 18:32:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Your code here
 let dodger = document.getElementById("dodger");
-dodger.style.backgroundColor = "#FF69B4";
-dodger.style.backgroundColor = "#FF69B4";
+dodger.style.backgroundColor = "#000000";
 
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace("px", "");
